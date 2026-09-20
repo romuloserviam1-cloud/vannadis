3cbd707b7590d88b767e0b555d657074670ef8fe
romuloserviam1-cloud <romuloserviam1@gmail.com>
2026-09-19 22:39:16 -0400

    Add files via upload

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // URL pública temporária do backend; não coloque credenciais aqui.
-window.VANNADIS_PUBLIC_CONFIG = Object.freeze({backendUrl: "https://charming-manor-century-realistic.trycloudflare.com"});
+window.VANNADIS_PUBLIC_CONFIG = Object.freeze({backendUrl: ""});
