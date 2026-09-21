ee62b0a469af20ffee8235f3a7ad407c24c090b9
romuloserviam1-cloud <romuloserviam1@gmail.com>
2026-09-21 14:38:20 -0400

    Add files via upload

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // URL pública temporária do backend; não coloque credenciais aqui.
-window.VANNADIS_PUBLIC_CONFIG = Object.freeze({backendUrl: ""});
+window.VANNADIS_PUBLIC_CONFIG = Object.freeze({backendUrl: "https://definitely-yale-mae-cancellation.trycloudflare.com"});
