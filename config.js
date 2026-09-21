f13263e2d63a9a7643977e986b11eb33ef9bf61f
romuloserviam1-cloud <romuloserviam1@gmail.com>
2026-09-21 14:23:50 -0400

    Add files via upload

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // URL pública temporária do backend; não coloque credenciais aqui.
-window.VANNADIS_PUBLIC_CONFIG = Object.freeze({backendUrl: "https://ski-facility-experience-bob.trycloudflare.com"});
+window.VANNADIS_PUBLIC_CONFIG = Object.freeze({backendUrl: ""});
